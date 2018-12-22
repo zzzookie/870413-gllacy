b17c5a6a17652c058bc822e18b3b5cf04929c1c3
zzzookie <iushulgin@gmail.com>
2018-12-22 19:07:37 +0300

    Добавлено модальное окно обратной связи

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+var link = document.querySelector(".feedback-link");
+var popup = document.querySelector(".modal-feedback");
+var background = document.querySelector(".modal-background");
+var close = popup.querySelector(".modal-close");
+var backgroundClose = document.querySelector(".modal-background-close");
+
+var form = document.querySelector("form");
+var login = document.querySelector("[name=login]");
+var password = document.querySelector("[name=password]");
+
+link.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  background.classList.add("modal-show");
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  background.classList.remove("modal-show");
+});
+
+backgroundClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  background.classList.remove("modal-show");
+});
+
+form.addEventListener("submit", function (evt) {
+	if (!login.value || !password.value) {
+		evt.preventDefault();
+		console.log("Не введён логин и пароль!");
+	}
+});
